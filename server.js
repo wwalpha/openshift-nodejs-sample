16b5ccd179fbe89f3dd289c64a3525210f1ce339
wwalpha <wwalpha@gmail.com>
2019-04-24 12:07:36 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'))
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log('Example app listening on port %s!', port));
